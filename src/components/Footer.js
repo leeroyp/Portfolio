4303cb0ac1093e352f01b2068357f3248ffcf34c
Leeroy Phili <leeroyphili@gmail.com>
2020-12-16 16:04:13 -0500

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,6 +23,8 @@ export default class Footer extends Component {
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
       </div>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
     </footer>
     );
   }
