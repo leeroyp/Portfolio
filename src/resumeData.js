fda41a1c5833ee3f3f3827598fa75b3751f0a3f6
Leeroy Phili <leeroyphili@gmail.com>
2020-12-24 00:20:25 -0500

    spelling and grammar checks

--- src/resumeData.js
+++ src/resumeData.js
@@ -28,12 +28,12 @@ let resumeData = {
           "className":"btn btn-secondary btn-sm"
         }
       ],
-    "aboutme":"Mechanical engineering technologist pursing a career as a software developer. Although during my undergrad studies I took some coding classes, it was not until I graduated that I knew software development was the right career path for me. In an effort to achieve this dream, I have since enrolled in a number of courses achieving multiple certificates as a full stack web developer and furthermore for frameworks such as React, React Native, Redux and more.  Understanding the importance of community contributions, Im a very active member with Toronto Js and I continue to seek challenging projects that will help acquire more  knowledge about the subject.   ",
+    "aboutme":"My name is Leeroy Phili. I recently graduated from a Full Stack web developer program at the University of Toronto. I'm currently looking for my first job as a developer. In the last few months since graduation, I have been completing personal projects than can be seen on my github page. In an effort to further my knowledge on the subject and stay current with the latest frameworks updates, I continue to take a number of courses on udemy during my freetime. Although, I have no experience in the field, I believe my drive, passion and determination about software developement can provide a fresh take on every project. I like new challenges and always open to constructive feedback. I am a quick learner and can comfortable adapt to any new environment . I am ready to use my skills and talent to be a team playeer to help build quality and highly function products.   ",
     "resume":"images/Leeroy-Phili-Resume.pdf",
     "projects":[
       {
         "name":"YoTixx ",
-        "projectDiscription":"Inspired by the likes of ticketmaster, stubhub and many more ticketing apps, I challenged myself to build an app that was similar while implementing cool features such as price compare for the same or similar tickets sold  across multiple platforms. I used stubhub, Ticketmaster and Gametime web Api  to fetch data needed. Using firebase Auth, each user is able to signup/signin to access other cool features like view current tickets, or change some privacy settings in the app. Overall the app has a lot potential also displays a deep knowledge of ReactNative",
+        "projectDiscription":"Inspired by the likes of ticketmaster, stubhub and many more ticketing apps. I challenged myself to build an app that was similar while implementing cool features such as price compare for the same or similar tickets sold  across multiple platforms. I used stubhub, Ticketmaster and Gametime web Api  to fetch data needed. For user authenticatication I used firebase Auth to enable each user to signup/signin to access other cool features like view current tickets, or change some privacy settings in the app. Overall the app has a lot potential also displays a deep knowledge of ReactNative",
         "framework":"ReactNative, Node js , Express, Expo, Mongo",
         "year": 2020,
         "imgurl":"images/portfolio/modals/TicketApp.png",
@@ -42,7 +42,7 @@ let resumeData = {
       },
       {
         "name":"Location-Tracker",
-        "projectDiscription":" This was one of my first mobile application. This app was build using expo for the purposes of understanding and learning more about react native. This app let user track their routes using maps, at the end of each route logged in users have the ability to save the track.  All tracks will be saved on the Mongo Database.  Each time a user logs in they have the ability to view the previous saved tracks. This was a fun knowledgable project. At the end of the project I had great understanding of React Native navigation and more familiarity with React hooks ",
+        "projectDiscription":" This was one of my first mobile application. This app was built using expo for the purposes of understanding and learning more about react native. This app let user track their routes using maps, logged in users have the ability record and save each route track with their own custom track name.  All route tracks will be saved on the Mongo Database.  Each time a user logs in they have the ability to view the previous saved tracks. This was a fun knowledgable project. At the end of the project I had great understanding of React Native navigation and more familiarity with React hooks such as useContext and more  ",
         "framework":"React Native , Node, Mongo",
         "year": 2020,
         "imgurl":"images/portfolio/modals/trackerApp.png",
@@ -52,7 +52,7 @@ let resumeData = {
      
       {
         "name":"Food App",
-        "projectDiscription":"This is one of my very first mobile apps build using Expo CLI. In this app user can search any food item in the search bar. When a user submits the search, a request is sent to the yelp api for a business search. The response data from the api is filtered by pricing and and rendered to the screen.  Users can click on any result to view more food items from the same restaurant. ",
+        "projectDiscription":"This app was  build using Expo CLI. Althought the app apppears to be very simple, it covers the most important topics of React Native.  In this app user can search any food item in the search bar which will send a request to the yelp api for a business search. The response data from the api is filtered by pricing and and rendered to the screen accordingly.  Users can click on any result to view more food items from the same restaurant. ",
         "framework":"Javascript, Expo CLI, React Native",
         "year": 2020,
         "imgurl":"images/portfolio/modals/food2.png",
@@ -62,7 +62,7 @@ let resumeData = {
       },
       {
         "name":"The weather dashboard ",
-        "projectDiscription":"This is a neat project that i really enjoyed building. Upon visit page, the user is instantley shown the current weather and the 5 day weather forecast of their curent city.  Users have the ability to search for any city in the world and get the the current weather and the 5 day weather forecast. As the user searches for different cities, each city is added to the array of previous searched cities and save in the local storage.  All the cities in the array are button which can be clicked to view the weather and the forecast of that particular city. All the weather data is fetched from Open Weather Api using AJAX    ",
+        "projectDiscription":"This is a neat project that I really enjoyed building. Upon visit page, the user is instantly shown the current weather and the 5 day weather forecast of their curent city.  Users have the ability to search for any city in the world and get the the current weather and the 5 day weather forecast. As the user searches for different cities, each city is added to the array of previous searched cities and save in the local storage.  All the previsouly searched city names are rendered below the search bar in the search history. Users can click on any city in the search history and again be presented with current and future conditions of that city. All the weather data is fetched from Open Weather Api using AJAX    ",
         "framework":"javascript,JQuery,AJAX, Node.js, Bootstrap, HTML5",
         "year": 2020,
         "imgurl":"images/portfolio/modals/weatherApp.png",
@@ -73,7 +73,7 @@ let resumeData = {
 
       {
         "name":"Eat Da Burger",
-        "projectDiscription":"Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat. Whenever a user submits a burger's name, the app will display the burger on the left side of the page waiting to be devoured. Each burger in the waiting area also has a Devour it! button. When the user clicks(devours the burger) it, the burger will move to the right side of the page, a user can devour as many burgers as they was.   All the burgers are stored  in MySql database, whether devoured or not.",
+        "projectDiscription":"Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat. Whenever a user submits a burger name, the app will display the burger on the left side of the page waiting to be devoured. Each burger in the waiting area also has a Devour it! button. When the user clicks(devours the burger), the burger will move to the right side of the page which indicates the burgers has been devoured, a user can devour as many burgers as they want.   All the burgers are stored in MySql database, whether devoured or not.",
         "framework":"Javascript,HTML, CSS, Node.js, MySql",
         "year": 2020,
         "imgurl":"images/portfolio/modals/burgerApp.png",
