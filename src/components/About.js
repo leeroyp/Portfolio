157230757cb7becfa040593e1b48736b3c079ba6
leeroy Phili <leeroy@myshoperon.com>
2021-04-27 14:46:27 -0400

    new images for skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,21 @@
 import React, { Component } from "react";
+
+import bootstrap from "../images/bootstrap.svg";
+import css from "../images/css.svg";
+import firebase from "../images/firebase.svg";
+import git from "../images/git.svg";
+import heroku from "../images/heroku.svg";
+import html from "../images/html.svg";
+import javascript from "../images/javascript.svg";
+import jquery from "../images/jquery.svg";
+import mongo from "../images/mongodb.svg";
+import node from "../images/nodejs.svg";
+import react from "../images/react.svg";
+import redux from "../images/redux.svg";
+import redux from "../images/redux.svg";
+import redux from "../images/redux.svg";
+import redux from "../images/redux.svg";
+
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -11,6 +28,20 @@ export default class About extends Component {
             </h1>
           </div>
         </div>
+
+        <div className="row skill">
+        <div className="nine columns main-col">
+            <h1>
+              <span>{resumeData.skillsDescription}</span>
+            </h1>
+            <div className="about_skills">
+              <img src={bootstrap} alt="Bootstrap css logo" />
+              <img src={bootstrap} alt="Bootstrap css logo" />
+              <img src={bootstrap} alt="Bootstrap css logo" />
+            </div>
+            </div>
+        </div>
+
         <div className="row">
           <div className="three columns">
             <img className="profile-pic" src="images/me.jpeg" alt="" />
@@ -45,6 +76,22 @@ export default class About extends Component {
                   })}
               </ul>
             </div> */}
+
+            
+            <div className="about_skills">
+              <img src={bootstrap} alt="Bootstrap css logo" />
+              <img src={css} alt="css logo" />
+              <img src={firebase} alt="Google firebase logo" />
+              <img src={git} alt="git logo" />
+              <img src={heroku} alt="heroku logo" />
+              <img src={html} alt="html logo" />
+              <img src={javascript} alt="javascript logo" />
+              <img src={jquery} alt="JQuery javascript library logo" />
+              <img src={mongo} alt="mongo logo" />
+              <img src={node} alt="node logo" />
+              <img src={react} alt="react logo" />
+              <img src={redux} alt="react logo" />
+            </div>
           </div>
         </div>
       </section>
